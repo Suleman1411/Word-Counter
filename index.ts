0cc1df2a75b3129bdd929ebe85ba92c7807d3075
Suleman pervez <119485959+Suleman1411@users.noreply.github.com>
2023-10-24 12:24:16 +0500

    10/24/2023

---
+++ index.ts
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+
+const promptUserForSentence = async () => {
+    const { Sentence } = await inquirer.prompt([
+        {
+            name: "Sentence",
+            type: "input",
+            message: "Enter a sentence to count the words:"
+        }
+    ]);
+    return Sentence;
+};
+
+const countWordsInSentence = (sentence: string) => {
+    const words = sentence.trim().split(" ");
+    return words.length;
+};
+
+const main = async () => {
+    const sentence = await promptUserForSentence();
+    const wordCount = countWordsInSentence(sentence);
+
+    if (wordCount === 1) {
+        console.log(`One word doesn't make a sentence: "${sentence}"`);
+    } else if (wordCount == 2) {
+        console.log(`Two words alone do not form a sentence: "${sentence}"`);
+    } else {
+        console.log(`Your sentence has ${wordCount} words: "${sentence}"`);
+    }
+};
+
+main();
